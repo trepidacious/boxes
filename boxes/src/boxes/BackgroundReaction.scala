72f25d6d3029d5279e5a788b08fbcc118570866c
Trepi Dacious <trepidacious@gmail.com>
2011-08-10 23:32:47 +0100

    Moved Responder to its own file in utils, since it is now generally useful.

--- boxes/src/boxes/BackgroundReaction.scala
+++ boxes/src/boxes/BackgroundReaction.scala
@@ -1,67 +1,7 @@
 package boxes
 
-import util.DaemonThreadFactory
 import java.util.concurrent.atomic.AtomicBoolean
-import java.util.concurrent.{CancellationException, Executor, Executors}
-
-object Responder {
-	val defaultExecutorService = Executors.newFixedThreadPool(4, new DaemonThreadFactory());
-
-  def apply(executor:Executor = defaultExecutorService) = new Responder(executor)
-}
-
-class Responder(val executor:Executor) {
-
-  private val responseLock = new Object()
-  private var nextResponse:Option[(AtomicBoolean => Unit)] = None //The next response function, if one is pending
-  private var currentlyRunning = false
-  private val shouldCancel = new AtomicBoolean(false)
-
-  def request(response: (AtomicBoolean => Unit)) {
-    responseLock.synchronized {
-      nextResponse = Some(response)
-
-      //Cancel current response, and make sure another runs immediately afterwards
-      if (currentlyRunning) {
-        shouldCancel.set(true)
-
-      //If we are not running, then we can service the update immediately
-      } else {
-        launchNewResponse()
-      }
-    }
-  }
-
-  //If required, schedule a new response
-  private def launchNewResponse() {
-    responseLock.synchronized {
-      nextResponse.foreach(response => {
-        nextResponse = None
-        shouldCancel.set(false)
-        executor.execute(new ResponseRunnable(response))
-        currentlyRunning = true
-      })
-    }
-  }
-
-  //Call response, then start another response immediately if needed
-  private class ResponseRunnable(val response:(AtomicBoolean => Unit)) extends Runnable {
-
-    override def run() {
-      try {
-        response.apply(shouldCancel)
-      } catch {
-        case e:Exception => e.printStackTrace()
-      } finally {
-        responseLock.synchronized {
-          currentlyRunning = false
-          launchNewResponse()
-        }
-      }
-    }
-  }
-
-}
+import util.Responder
 
 class BackgroundReaction(responseSource: => (AtomicBoolean => Unit)) extends Reaction {
 
